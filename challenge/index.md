903b235a1622a02e6f491ce990d685cd9cd1005d
Maya Kappen <mayakappen@gmail.com>
2022-04-18 20:30:21 -0600

    Initial commit

---
+++ challenge/index.md
@@ -0,0 +1,8 @@
+<h1> Top 5 Jane Austen Books <h2>
+1. Pride and Prejudice
+2. Emma
+3. Sense and Sensibility
+4. Mansfield Park
+5. Northanger Abbey
+
+We discussed each question and explained what we were doing as we were working, keeping relatively the same pace throughout. The part we got kind of stuck on was step 3 if we were supposed to create a new file or find one in the directory with that name, we decided to touch create a new file. Another question was if comments and annotation meant different things, my partner helped clarify that.
---
+++ challenge/janeAusten.js
@@ -0,0 +1,13 @@
+//I named the file janeAusten because it lists facts and information about her
+var author = "Jane Austen";
+var numBooks = 6;
+//She published 6 books
+var isAlive = false;
+//Jane Austen is dead
+var birthday = "December 16 1775";
+//wasn't sure how to format date as a number so stored it as a string
+var died = "July 18 1817";
+var country = "England";
+//she lived there all her life
+var similarAuthors = ["Emily Bronte", "Charles Dickens", "Virginia Woolf", "Mary Shelley", "Louisa May Alcott", "Harper Lee"];
+//not necisarrily similar in style but relating to time period and subject matter
